8c31666b8a0ad0edbf422e35e395adc1fd954729
Yeison <enmanuelquiterios@gmail.com>
2021-09-26 16:31:39 -0400

    commit tree b7160cafd861c097762c1c57293272de5afa94c2

--- index.js
+++ index.js
@@ -1,61 +1,49 @@
 // Your code here
 
-const dodger = document.getElementById('dodger');
-dodger.style.backgroundColor= '#FFB830'
-dodger.style.bottom = '0px';
-dodger.style.right = '180px';
-dodger.style.left = '180px'
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "#FFB830";
+dodger.style.bottom = "0px";
+dodger.style.right = "180px";
+dodger.style.left = "180px";
 
+function moveDodgerLeft() {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      e.preventDefault();
+      const leftNumber = dodger.style.left.replace("px", "");
+      let left = parseInt(leftNumber, 10);
+      console.log(left);
+      if (left >= 2 && left < 358) {
+        dodger.style.left = `${left - 1}px`;
+      }
 
-
-
-
-function moveDodgerLeft(){
-    window.addEventListener("keydown",  function(e){
-        if(e.key === 'ArrowLeft'){
-            e.preventDefault()
-            const leftNumber =dodger.style.left.replace('px', '');
-            let left = parseInt(leftNumber, 10);
-            console.log(left)
-            if (left >= 1 && left < 358 ) {
-                dodger.style.left = `${left - 1}px`;
-              }
-
-
-        let newPosition = dodger.style.left
-        newPosition = parseInt(newPosition)
-        console.log(newPosition)
-           
-        }
-    
-    })
-  
- }
-
-function moveDodgerRight(){
-    window.addEventListener("keydown", function(e){
-        if(e.key === "ArrowRight"){
-            e.preventDefault()
-            const leftNumber = dodger.style.left.replace('px', '');
-            let left = parseInt(leftNumber, 10);
-            console.log(left)
-            if(left >= 1  && left < 358){
-                dodger.style.left = `${left + 1}px`
-            }
-        }
-        let newPosition = dodger.style.left
-        newPosition = parseInt(newPosition)
-        console.log(newPosition, `New position`)
-     
-    })
-
+      let leftNew = dodger.style.left;
+      leftNew = parseInt(leftNew);
+      console.log(leftNew);
+    }
+  });
 }
 
-
+function moveDodgerRight() {
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      e.preventDefault();
+      const leftNumber = dodger.style.left.replace("px", "");
+      let left = parseInt(leftNumber, 10);
+      console.log(left);
+      if (left >= 2 && left < 358) {
+        dodger.style.left = `${left + 1}px`;
+      }
+    }
+    let leftNew = dodger.style.left;
+    leftNew = parseInt(leftNew);
+    console.log(leftNew, `New position`);
+  });
+}
 
 //Invoking my functions here;
 
 moveDodgerLeft();
 moveDodgerRight();
 
-///Global move function 
+///Global move function
